bb68a882eada08b1d2223f7e95b1b7a999aecd3d
Göksu Alkan <77745630+goksu1@users.noreply.github.com>
2022-10-04 22:27:39 +0300

    düzenleme yapıldı.

--- myapp/src/pages/Register/RegisterPage.tsx
+++ myapp/src/pages/Register/RegisterPage.tsx
@@ -3,18 +3,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { RegisterForm } from "../../components/forms";
 import { RegisterFormProps } from "../../components/forms/RegisterForm/RegisterForm.types";
+import { auth } from "../../services/http/endpoints/auth";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
   const handleRegister: RegisterFormProps["onRegister"] = (values) => {
-    axios
-      .post("http://localhost:80/auth/register", values)
-      .then((res) => {
-        if (res.status === 200) {
-          navigate("/login");
-        }
-      })
-      .catch((err) => console.log(err));
+   auth.register(values).then((res)=>{
+    console.log('res', res)
+    navigate("/login")
+   })
   };
   return (
     <div style={{ marginTop: "10vh" }}>
